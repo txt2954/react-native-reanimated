377eaef3dcbdaba034b43f1c73fddcf48eff09ae
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2024-07-01 12:40:52 +0200

    Fix variable name

--- packages/react-native-reanimated/plugin/src/workletFactory.ts
+++ packages/react-native-reanimated/plugin/src/workletFactory.ts
@@ -228,7 +228,7 @@ export function makeWorkletFactory(
               ? objectProperty(
                   identifier(variable.name),
                   memberExpression(
-                    identifier(variable.name.slice(0, -13)),
+                    identifier(variable.name.slice(0, -12)),
                     identifier(variable.name)
                   )
                 )
